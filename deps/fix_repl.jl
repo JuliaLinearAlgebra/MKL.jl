22c7d5ec9060de442f3cd91cdfb11151d564b6d1
daviehh <25255906+daviehh@users.noreply.github.com>
2019-11-30 10:33:53 -0500

    release mode

--- deps/fix_repl.jl
+++ deps/fix_repl.jl
@@ -7,4 +7,5 @@ prec_jl_content = read(prec_jl_fn, String)
 open(prec_jl_fn, "w") do f
    write(f, replace(prec_jl_content, "@assert n_succeeded > 3500" => raw"println(\"processed: $n_succeeded\") # @assert n_succeeded > 3500"))
 end
-PackageCompiler.build_sysimg(PackageCompiler.default_sysimg_path(), "generate_precompile.jl")
+
+PackageCompiler.build_sysimg(PackageCompiler.default_sysimg_path(), prec_jl_fn, verbose=true, cpu_target="native", release=true)
