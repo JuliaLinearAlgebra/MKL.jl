262171842a1f5bf4d5f900b8801c00c62d66727f
daviehh <25255906+daviehh@users.noreply.github.com>
2019-12-07 14:55:55 -0500

    fix use of hard-coded version number

--- deps/fix_repl.jl
+++ deps/fix_repl.jl
@@ -1,6 +1,12 @@
 using PackageCompiler
 
-prec_jl_url = "https://raw.githubusercontent.com/JuliaLang/julia/release-1.3/contrib/generate_precompile.jl"
+
+jl_dev_ver = length(VERSION.prerelease) == 2 && (VERSION.prerelease)[1] == "DEV" # test if running nightly/unreleased version
+
+jl_gh_tag = jl_dev_ver ? "master" : "release-$(VERSION.major).$(VERSION.minor)"
+
+prec_jl_url = "https://raw.githubusercontent.com/JuliaLang/julia/$jl_gh_tag/contrib/generate_precompile.jl"
+
 prec_jl_fn = "generate_precompile.jl"
 download(prec_jl_url, prec_jl_fn)
 prec_jl_content = read(prec_jl_fn, String)
