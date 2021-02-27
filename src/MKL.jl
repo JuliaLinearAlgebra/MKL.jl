bffa2e2091f01a6ff11c1ddb9b597791f51f0ab7
Viral B. Shah <viral@mayin.org>
2021-02-27 10:02:58 -0500

    Run on Julia 1.6 (by building sysimg) and 1.7 (with LBT) in the same codebase

--- src/MKL.jl
+++ src/MKL.jl
@@ -1,12 +1,12 @@
 module MKL
 
 using MKL_jll
+VERSION > v"1.6" && using LinearAlgebra
 
-# We cannot depend on LinearAlgebra since we need to be loaded before it
 if Base.USE_BLAS64
-    const BlasInt = Int64
+    const MKLBlasInt = Int64
 else
-    const BlasInt = Int32
+    const MKLBlasInt = Int32
 end
 
 @enum Threading begin
@@ -16,6 +16,7 @@ end
     THREADING_GNU
     THREADING_TBB
 end
+
 @enum Interface begin
     INTERFACE_LP64
     INTERFACE_ILP64
@@ -23,34 +24,31 @@ end
 end
 
 function set_threading_layer(layer::Threading = THREADING_INTEL)
-    err = ccall((:MKL_Set_Threading_Layer, libmkl_rt), Cint,
-        (Cint,), layer)
-    if err == -1
-        throw(ErrorException("return value was -1"))
-    end
+    err = ccall((:MKL_Set_Threading_Layer, libmkl_rt), Cint, (Cint,), layer)
+    err == -1 && throw(ErrorException("return value was -1"))
     return nothing
 end
 
-function set_interface_layer(interface = BlasInt == Int64 ? INTERFACE_ILP64 : INTERFACE_LP64)
-    err = ccall((:MKL_Set_Interface_Layer, libmkl_rt), Cint,
-        (Cint,), interface)
-    if err == -1
-        throw(ErrorException("return value was -1"))
-    end
+function set_interface_layer(interface = Base.USE_BLAS64 ? INTERFACE_ILP64 : INTERFACE_LP64)
+    err = ccall((:MKL_Set_Interface_Layer, libmkl_rt), Cint, (Cint,), interface)
+    err == -1 && throw(ErrorException("return value was -1"))
     return nothing
 end
 
 function __init__()
-    set_threading_layer()
-    set_interface_layer()
+    if MKL_jll.is_available()
+        set_threading_layer()
+        set_interface_layer()
+        VERSION > v"1.6" && BLAS.lbt_forward(libmkl_rt, clear=true)
+    end
 end
 
-mklnorm(x::Vector{Float64}) = ccall((:dnrm2_, libmkl_rt), Float64,
-    (Ref{BlasInt}, Ptr{Float64}, Ref{BlasInt}),
-    length(x), x, 1)
-
-
-# Include installation routines as well
-include("install.jl")
-
+function mklnorm(x::Vector{Float64})
+    ccall((:dnrm2_, libmkl_rt), Float64,
+          (Ref{MKLBlasInt}, Ptr{Float64}, Ref{MKLBlasInt}),
+          length(x), x, 1)
 end
+
+VERSION > v"1.6" && include("install.jl")
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,12 @@
-using Test
 using LinearAlgebra
+using MKL
+using MKL_jll
+using Test
 
-@test BLAS.vendor() == :mkl
+if VERSION > v"1.6"
+    @test BLAS.get_config().loaded_libs[1].libname == libmkl_rt
+else
+    @test BLAS.vendor == :mkl
+end
+
+@test LinearAlgebra.peakflops() > 0
