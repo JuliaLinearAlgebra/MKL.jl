fc6a5c2b136f46058f0b06506399d330de1f8338
Viral B. Shah <viral@mayin.org>
2021-03-05 17:07:41 -0500

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,11 @@
 using LinearAlgebra
 using MKL
-using MKL_jll
 using Test
 
-if VERSION > v"1.7.0-DEV.623"
+if VERSION > MKL.JULIA_VER_NEEDED
     @test BLAS.get_config().loaded_libs[1].libname == libmkl_rt
 else
     @test BLAS.vendor() == :mkl
 end
 
-@test LinearAlgebra.peakflops() > 0
+@test peakflops() > 0
