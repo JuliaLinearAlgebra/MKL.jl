8ea528fb5dfdab9b6185eef51a8c93c5831d4927
Kristoffer <kcarlsson89@gmail.com>
2022-02-16 20:08:18 +0100

    transition to only supporting Julia 1.7+

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,7 @@ using MKL, MKL_jll, Test
 import LinearAlgebra
 
 @show LinearAlgebra.BLAS.get_config()
-
-if VERSION > MKL.JULIA_VER_NEEDED
-    @test LinearAlgebra.BLAS.get_config().loaded_libs[1].libname == libmkl_rt
-else
-    @test LinearAlgebra.BLAS.vendor() == :mkl
-end
-
+@test LinearAlgebra.BLAS.get_config().loaded_libs[1].libname == libmkl_rt
 @test LinearAlgebra.peakflops() > 0
 
 include(joinpath(Sys.STDLIB, "LinearAlgebra", "test", "runtests.jl"))
